d9ada8a96996df2cd0354f9ec88cde6128092c6a
Rodrigo Abreu <rodrigo.s.abreu@gmail.com>
2023-01-25 17:02:33 -0300

    fix: ajustando as constantes

--- STS_ConsoleApp/Constants.cs
+++ STS_ConsoleApp/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string UID = "AKIA3VSLMYEZAHVDUTOC";
-        public const string Secret = "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";
+        public const string UID = "***";
+        public const string Secret = "***";
     }
 }
