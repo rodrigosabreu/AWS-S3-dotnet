ba47a4107f4e6d080875aa8f39e86797669f7f01
Rodrigo Abreu <rodrigo.s.abreu@gmail.com>
2023-01-25 16:58:43 -0300

    feat: subindo o projeto para o git

---
+++ STS_ConsoleApp/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace STS_ConsoleApp
+{
+    public static class Constants
+    {
+        public const string UID = "AKIA3VSLMYEZAHVDUTOC";
+        public const string Secret = "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";
+    }
+}
